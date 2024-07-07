17b99740df372abfe9b6f6d11f8c82955575cc78
Mahdis <MMMJJJJJ@gmail.com>
2024-07-07 13:10:12 +0330

    file content changed

--- lu-project-master/project/final/main.py
+++ lu-project-master/project/final/main.py
@@ -32,10 +32,24 @@ def create_student(student: schemas.Student, db: Session = Depends(get_db)):
     print(db_student)
     if db_student:
         raise HTTPException(status_code=400, detail="student already registered")
+    schemas.validate_student(student)
+    error_akhs_dars = {}
+    scourseids = student.scourseids.split(",")
+    for code in scourseids:
+        db_rel_course_student = crud.get_course(db, Course_id=code)
+        if db_rel_course_student is None:
+            error_akhs_dars["lcourseids"] = "کد درس انتخاب شده جزو دروس اريه شده نمی باشد"
+    lids = student.lids.split(",")
+    for code in lids:
+        db_rel_PS = crud.get_ostad(db, ostad_id=code)
+        if db_rel_PS is None:
+            error_akhs_dars["lids"] = "استاد انتخابی شما در سیستم موجود نمی باشد"
+    if error_akhs_dars:
+        raise HTTPException(detail=error_akhs_dars , status_code=400)
     return crud.create_student(db=db, student=student)
 
 
-@app.get("/Getstudent/{student_id}", response_model=schemas.Student)
+@app.get("/Getstudent/{student_id}", response_model=schemas.Student_response)
 def read_student(student_id: int, db: Session = Depends(get_db)):
     db_student = crud.get_student(db, student_id=student_id)
     if db_student is None:
@@ -47,12 +61,26 @@ def update_student(student_id: str, student: schemas.Student, db: Session = Depe
     db_student = crud.update_student(db, student_id, student)
     if db_student is None:
         raise HTTPException(status_code=404, detail="Student not found")
+    schemas.validate_student(student)
+    error_akhs_dars = {}
+    scourseids = student.scourseids.split(",")
+    for code in scourseids:
+        db_rel_course_student = crud.get_course(db, Course_id=code)
+        if db_rel_course_student is None:
+            error_akhs_dars["lcourseids"] = "کد درس انتخاب شده جزو دروس اريه شده نمی باشد"
+    lids = student.lids.split(",")
+    for code in lids:
+        db_rel_PS = crud.get_ostad(db, ostad_id=code)
+        if db_rel_PS is None:
+            error_akhs_dars["lids"] = "کد استاد انتخاب شده جزو اساتید نمی باشد"
+    if error_akhs_dars:
+        raise HTTPException(detail=error_akhs_dars , status_code=400)
     return db_student
 
 
     
 @app.delete("/Delstudent/{student_id}", response_model=schemas.Student)
-def read_student(student_id: int, db: Session = Depends(get_db)):
+def del_student(student_id: int, db: Session = Depends(get_db)):
     db_student = crud.get_student(db, student_id=student_id)
     if db_student is None:
         raise HTTPException(status_code=404, detail="student not found")
@@ -63,42 +91,60 @@ def read_student(student_id: int, db: Session = Depends(get_db)):
 
 
 
-#professor
+#ostad
 
-@app.post("/Createprofessor/", response_model=schemas.Professor)
-def create_professor(professor: schemas.Professor, db: Session = Depends(get_db)):
-    db_professor = crud.get_professor(db, Professor_id=professor.lid)
-    print(db_professor)
-    if db_professor:
-        raise HTTPException(status_code=400, detail="Professor already registered")
-    return crud.create_professoe(db=db, professor=professor)
-
-
-@app.get("/Getprofessor/{professor_id}", response_model=schemas.Professor)
-def read_professor(professor_id: int, db: Session = Depends(get_db)):
-    db_professor = crud.get_professor(db, Professor_id=professor_id)
-    if db_professor is None:
-        raise HTTPException(status_code=404, detail="professor not found")
-    return db_professor
-
-
-
-@app.put("/prefessor/{prefessor_id}", response_model=schemas.Professor)
-def update_prefessor(prefessor_id: str, prefessor: schemas.Professor, db: Session = Depends(get_db)):
-    db_prefessor = crud.update_prefessor(db, prefessor_id, prefessor)
-    if db_prefessor is None:
-        raise HTTPException(status_code=404, detail="prefessor not found")
-    return db_prefessor
+@app.post("/Createostad/", response_model=schemas.ostad)
+def create_ostad(ostad: schemas.ostad, db: Session = Depends(get_db)):
+    db_ostad = crud.get_ostad(db, ostad_id=ostad.lid)
+    print(db_ostad)
+    if db_ostad:
+        raise HTTPException(status_code=400, detail="ostad already registered")
+    schemas.validate_ostad(ostad)
+    lcourseids = ostad.lcourseids.split(",")
+    error_akhs_dars = {}
+    for code in lcourseids:
+        db_rel_course_ostad = crud.get_course(db, Course_id=code)
+        if db_rel_course_ostad is None:
+            error_akhs_dars["lcourseids"] = "کد درس انتخاب شده جزو دروس اريه شده نمی باشد"
+    if error_akhs_dars:
+        raise HTTPException(detail=error_akhs_dars , status_code=400)
+    return crud.create_professoe(db=db, ostad=ostad)
 
 
+@app.get("/Getostad/{ostad_id}", response_model=schemas.ostad_response)
+def read_ostad(ostad_id: int, db: Session = Depends(get_db)):
+    db_ostad = crud.get_ostad(db, ostad_id=ostad_id)
+    if db_ostad is None:
+        raise HTTPException(status_code=404, detail="ostad not found")
+    return db_ostad
 
-@app.get("/Delprofessor/{professor_id}", response_model=schemas.Professor)
-def del_professor(professor_id: int, db: Session = Depends(get_db)):
-    db_professor = crud.get_professor(db, Professor_id=professor_id)
-    if db_professor is None:
-        raise HTTPException(status_code=404, detail="professor not found")
-    crud.removeprefessor(db , Professor_id=professor_id)
-    return db_professor
+
+
+@app.put("/ostad/{ostad_id}", response_model=schemas.ostad)
+def update_ostad(ostad_id: str, ostad: schemas.ostad, db: Session = Depends(get_db)):
+    db_ostad = crud.update_ostad(db, ostad_id, ostad)
+    if db_ostad is None:
+        raise HTTPException(status_code=404, detail="ostad not found")
+    schemas.validate_ostad(ostad)
+    error_akhs_dars = {}
+    lcourseids = ostad.lcourseids.split(",")
+    for code in lcourseids:
+        db_rel_course_ostad = crud.get_course(db, Course_id=code)
+        if db_rel_course_ostad is None:
+            error_akhs_dars["lcourseids"] = "کد درس انتخاب شده جزو دروس اريه شده نمی باشد"
+    if error_akhs_dars:
+        raise HTTPException(detail=error_akhs_dars , status_code=400)
+    return db_ostad
+
+
+
+@app.delete("/Delostad/{ostad_id}", response_model=schemas.ostad)
+def del_ostad(ostad_id: int, db: Session = Depends(get_db)):
+    db_ostad = crud.get_ostad(db, ostad_id=ostad_id)
+    if db_ostad is None:
+        raise HTTPException(status_code=404, detail="ostad not found")
+    crud.removeostad(db , ostad_id=ostad_id)
+    return db_ostad
     
 
 
@@ -111,6 +157,7 @@ def create_course(course: schemas.Course, db: Session = Depends(get_db)):
     print(db_course)
     if db_course:
         raise HTTPException(status_code=400, detail="Course already registered")
+    schemas.validate_course(course)
     return crud.create_cource(db=db, course=course)
 
 
@@ -124,20 +171,20 @@ def read_course(course_id: int, db: Session = Depends(get_db)):
 
 
 
-@app.put("/course/{course_id}", response_model=schemas.Course)
-def update_course(course_id: str, course: schemas.Course, db: Session = Depends(get_db)):
-    db_course = crud.update_course(db, course_id, course)
+@app.put("/course/{courseid}", response_model=schemas.Course)
+def update_course(courseid: str, course: schemas.Course, db: Session = Depends(get_db)):
+    schemas.validate_course(course)
+    db_course = crud.update_course(db, courseid, course)
     if db_course is None:
         raise HTTPException(status_code=404, detail="course not found")
     return db_course
 
 
 
-@app.get("/Delcourse/{course_id}", response_model=schemas.Course)
-def read_course(course_id: int, db: Session = Depends(get_db)):
+@app.delete("/Delcourse/{course_id}", response_model=schemas.Course)
+def del_course(course_id: int, db: Session = Depends(get_db)):
     db_course = crud.get_course(db, Course_id=course_id)
     if db_course is None:
         raise HTTPException(status_code=404, detail="Course not found")
     crud.removecourse(db, Course_id=course_id)
     return db_course
-
